c5f288bcae7b92539557a43d00ba538e72528b6a
amiralivlp021-jpg <amiralivlp021@gmail.com>
2026-06-23 17:32:23 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,12 +2,13 @@ import asyncio
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
-TOKEN = "توکن جدید ربات"
+TOKEN = "8746235673:AAEIf0rUGBdSsxtrGLj9J-h2M8Wtg5_FoN4"
 
 keyboard = [
     ["درباره ما", "تماس با ما"],
     ["سفارش"]
 ]
+
 reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -26,6 +27,7 @@ async def main():
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     print("ربات روشن شد...")
+
     await app.initialize()
     await app.start()
     await app.updater.start_polling()
