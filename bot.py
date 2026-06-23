005247f8cfbc155fbb9a7d8c1de6459ff798fb5c
amiralivlp021-jpg <amiralivlp021@gmail.com>
2026-06-23 17:25:48 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
+import asyncio
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
-TOKEN="8746235673:AAEIf0rUGBdSsxtrGLj9J-h2M8Wtg5_FoN4"
+TOKEN = "توکن جدید ربات"
 
 keyboard = [
     ["درباره ما", "تماس با ما"],
@@ -11,33 +12,26 @@ reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "سلام 👋\nبه ربات خوش اومدی.\nیکی از گزینه‌ها رو انتخاب کن:",
+        "سلام 👋\nبه ربات خوش اومدی.",
         reply_markup=reply_markup
     )
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text
+    await update.message.reply_text(update.message.text)
 
-    if text == "درباره ما":
-        await update.message.reply_text("ما یک فروشگاه/مجموعه خدماتی هستیم و خوشحالیم که کنار شما هستیم 🌷")
-
-    elif text == "تماس با ما":
-        await update.message.reply_text("شماره تماس: 0912xxxxxxx\nآیدی پشتیبانی: @yourid")
-
-    elif text == "سفارش":
-        await update.message.reply_text("برای ثبت سفارش، لطفاً نام محصول یا درخواستت رو برام بفرست ✍️")
-
-    else:
-        await update.message.reply_text(f"پیامت دریافت شد:\n{text}")
-
-def main():
+async def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     print("ربات روشن شد...")
-    app.run_polling()
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
+
+    while True:
+        await asyncio.sleep(3600)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
